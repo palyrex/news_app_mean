f7935c71c463bf47fd75c3501a909d8c912f2d7f
Pavel Redics <pavel.redics@gmail.com>
2015-01-25 23:13:24 +0000

    posts ctrl, inline templates, addComment function

--- app.js
+++ app.js
@@ -1,13 +1,18 @@
 angular.module('angularNews', ['ui.router'])
 .config([
 '$stateProvider',
-'$$urlRouteProvider',
+'$urlRouteProvider',
 function($stateProvider, $urlRouteProvider) {
   $stateProvider
   .state('home', {
     url: '/home',
     templateUrl: '/home.html',
     controller: 'MainCtrl'
+  })
+  .state('posts', {
+    url: '/posts{id}',
+    templateUrl: '/posts.html',
+    controller: 'PostsCtrl'
   });
 
   $urlRouteProvider.otherwise('home');
@@ -34,12 +39,34 @@ function($scope, posts){
     $scope.posts.push({
       title: $scope.title, 
       link: $scope.link, 
-      upvotes: 0});
+      upvotes: 0,
+      comments:[
+        {author: 'Joe', body: 'Cool post!', upvotes: 0},
+        {author: 'Bob', body: 'Great idea but everything is wrong!', upvotes: 0}
+        ]
+      });
     $scope.title = "";
     $scope.link = "";
   };
 
   $scope.incrementUpvotes = function(post) {
     post.upvotes += 1;
+  };
+}])
+.controller('PostsCtrl', [
+'$scope',
+'$stateParams',
+'posts',
+function($scope, $stateParams, posts) {
+  $scope.posts = posts.posts[$stateParam.id];
+
+  $scope.addComment = function() {
+    if($scope.body ==='') { return; }
+    $scope.posts.comments.push({
+      body: $scope.body,
+      author: 'user',
+      upvotes: 0
+    });
+    $scope.body = '';
   };
 }]);
