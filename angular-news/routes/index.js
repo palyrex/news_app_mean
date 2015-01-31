2b86ab1bebc484a2090c02ee182390de00afe578
Pavel Redics <pavel.redics@gmail.com>
2015-01-31 21:09:45 +0000

    created get and post routes

--- angular-news/routes/index.js
+++ angular-news/routes/index.js
@@ -1,9 +1,21 @@
-var express = require('express');
-var router = express.Router();
+var mongoose = require('mongoose');
+var Post = mongoose.model('Post');
+var Comment = mongoose.model('Comment');
 
-/* GET home page. */
-router.get('/', function(req, res) {
-  res.render('index', { title: 'Express' });
+router.get('/posts', function(req, res, next) {
+	Post.find(function(err, posts) {
+		if(err){return next(err);}
+
+		res.json(posts);
+	});
 });
 
-module.exports = router;
+router.post('posts', function(req, res, next) {
+	var post = new Post(req.body);
+
+	post.save(function(err, post) {
+		if(err){return next(err);}
+
+		res.json(post);
+	});
+});
