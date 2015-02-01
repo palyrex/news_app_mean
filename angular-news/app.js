901fe4f94e3491796a9668f061bc2a3b8eef6dbd
Pavel Redics <pavel.redics@gmail.com>
2015-02-01 23:39:22 +0000

    upvoting post

--- angular-news/app.js
+++ angular-news/app.js
@@ -5,9 +5,6 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
-var routes = require('./routes/index');
-var users = require('./routes/users');
-
 var app = express();
 
 //set up mongoose to use with mongodb
@@ -15,6 +12,28 @@ var mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost/news');
 require('./models/Posts.js');
 require('./models/Comments.js');
+
+
+var Post = mongoose.model('Post');
+var Comment = mongoose.model('Comment');
+
+app.get('/posts', function(req, res, next) {
+    Post.find(function(err, posts) {
+        if(err){return next(err);}
+
+        res.json(posts);
+    });
+});
+
+app.post('/posts', function(req, res, next) {
+    var post = new Post(req.body);
+
+    post.save(function(err, post) {
+        if(err){return next(err);}
+
+        res.json(post);
+    });
+});
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -27,9 +46,6 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-
-app.use('/', routes);
-app.use('/users', users);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- angular-news/routes/index.js
+++ angular-news/routes/index.js
@@ -2,7 +2,7 @@ var mongoose = require('mongoose');
 var Post = mongoose.model('Post');
 var Comment = mongoose.model('Comment');
 
-router.get('/posts', function(req, res, next) {
+app.get('/posts', function(req, res, next) {
 	Post.find(function(err, posts) {
 		if(err){return next(err);}
 
@@ -10,7 +10,7 @@ router.get('/posts', function(req, res, next) {
 	});
 });
 
-router.post('posts', function(req, res, next) {
+app.post('/posts', function(req, res, next) {
 	var post = new Post(req.body);
 
 	post.save(function(err, post) {
@@ -18,4 +18,20 @@ router.post('posts', function(req, res, next) {
 
 		res.json(post);
 	});
+});
+
+app.param('post', function(req, res, next, id) {
+	var query = Post.findById(id);
+
+	query.exec(function(err, post) {
+		if(err){return next(err);}
+		if(!post){return next(new Error("Can't find post"));}
+
+		req.post = post;
+		return next();
+	});
+});
+
+app.get('/posts/:post', function(req, res) {
+	res.json(req.post);
 });
